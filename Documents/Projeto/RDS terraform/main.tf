65db35a0e99ab7e9443e706889b73f0317901efa
Vinicius <viniciushrqvieira@gmail.com>
2025-05-06 12:56:20 -0300

    feat: adicionando infra ECS Fargate

--- Documents/Projeto/RDS terraform/main.tf
+++ Documents/Projeto/RDS terraform/main.tf
@@ -31,6 +31,12 @@ module "security_groups" {
       to_port     = 22
       protocol    = "tcp"
       cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+    cidr_blocks = "0.0.0.0/0"
     }
   ]
   egress_with_cidr_blocks = [
@@ -86,4 +92,72 @@ variable "region" {
 
 output "ec2_public_ip" {
   value = aws_instance.app_server.public_ip
+}
+
+resource "aws_ecr_repository" "nginx_repo" {
+  name = "nginx-container"
+  
+}
+
+resource "aws_ecs_cluster" "meu_cluster" {
+  name = "meu-cluster"  
+}
+
+resource "aws_ecs_task_definition" "nginx_task" {
+  family = "nginx-task"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu = "256" # 0.25 vCPU
+  memory = "512" # 512MB
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name = "nginx"
+      image = "nginx:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          protocol = "tcp"
+        }
+      ]
+    }
+  ])
+  
+}
+resource "aws_ecs_service" "nginx_service" {
+  name = "nginx-service"
+  cluster = aws_ecs_cluster.meu_cluster.id
+  task_definition = aws_ecs_task_definition.nginx_task.arn
+  launch_type = "FARGATE"
+  desired_count = 1
+
+  network_configuration {
+    subnets = module.vpc.public_subnets
+    assign_public_ip = true
+    security_groups = [module.security_groups.security_group_id]
+  }
+  depends_on = [ aws_iam_role_policy_attachment.ecs_task_execution_role_policy ]
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  
 }
